1d3bbad5b8e4a031d3e59346b8d6cc4ae01690c4
danmandel <danmandel@users.noreply.github.com>
2016-06-14 21:39:07 -0400

    Create solution.js

---
+++ first-factorial/solution.js
@@ -0,0 +1,15 @@
+function FirstFactorial(num) { 
+  var result = 1;
+  for (var i=1;i<num+1;i++){
+      result *= i;
+  }
+  return result; 
+}
+
+FirstFactorial(readline());                            
+
+
+
+
+
+
